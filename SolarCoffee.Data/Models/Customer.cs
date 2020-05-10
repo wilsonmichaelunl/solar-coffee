60e4575184df3c463e95ee49b4baf21d9d7f9156
Michael Wilson <wilson.michael@huskers.unl.edu>
2020-05-09 19:51:32 -0500

    add makefile, migrations

---
+++ SolarCoffee.Data/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarCoffee.Data.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public CustomerAddress PrimaryAddress { get; set; }
+    }
+}
---
+++ SolarCoffee.Data/SolarDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SolarCoffee.Data.Models;
+
+namespace SolarCoffee.Data
+{
+    public class SolarDBContext : IdentityDbContext 
+    {
+        public SolarDBContext() {}
+
+        public SolarDBContext(DbContextOptions options) : base(options) { }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<CustomerAddress> CustomerAddresses { get; set; }
+    }
+}
