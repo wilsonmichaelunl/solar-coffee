9a2f7553c6545069c9894daccccf2e83df51557a
Michael Wilson <wilson.michael@huskers.unl.edu>
2020-05-16 09:46:09 -0500

    implement data mapper

---
+++ SolarCoffee.Web/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SolarCoffee.Services.Product;
+using SolarCoffee.Web.Serialization;
+
+namespace SolarCoffee.Web.Controllers
+{
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IProductService _productService;
+
+        public ProductController(ILogger<ProductController> logger, IProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        [HttpGet("/api/product")]
+        public ActionResult GetProduct()
+        {
+            _logger.LogInformation("Getting all products");
+            var products = _productService.GetAllProducts();
+            var productViewModels = products
+                .Select(ProductMapper.SerializeProductModel);
+            return Ok(products);
+        }
+    }
+}
---
+++ SolarCoffee.Web/Serialization/ProductMapper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using SolarCoffee.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolarCoffee.Web.Serialization
+{
+    public class ProductMapper
+    {
+        /// <summary>
+        /// Maps a Product data model to a ProductModel view model
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public static ProductModel SerializeProductModel(Data.Models.Product product)
+        {
+            return new ProductModel
+            {
+                Id = product.Id,
+                CreatedOn = product.CreatedOn,
+                UpdatedOn = product.UpdatedOn,
+                Price = product.Price,
+                Name = product.Name,
+                Description = product.Description,
+                IsTaxable = product.IsTaxable,
+                IsArchived = product.IsArchived
+            };
+        }
+
+        /// <summary>
+        /// Maps a ProductModel view model to a Product data model
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public static Data.Models.Product SerializeProductModel(ProductModel product)
+        {
+            return new Data.Models.Product
+            {
+                Id = product.Id,
+                CreatedOn = product.CreatedOn,
+                UpdatedOn = product.UpdatedOn,
+                Price = product.Price,
+                Name = product.Name,
+                Description = product.Description,
+                IsTaxable = product.IsTaxable,
+                IsArchived = product.IsArchived
+            };
+        }
+    }
+}
